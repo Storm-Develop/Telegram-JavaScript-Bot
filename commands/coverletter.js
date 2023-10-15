d8393bbe808408e8b4b3e0610960b8d6debb283f
Vlad <vladsamonin@gmail.com>
2023-10-14 19:52:19 -0500

    Update coverletter.js

--- commands/coverletter.js
+++ commands/coverletter.js
@@ -3,7 +3,6 @@ const { OpenAIApi } = require('openai');
 // Replace with your OpenAI API key
 const openaiApiKey = process.env.OPENAI_TOKEN;
 
-
 module.exports = {
   name: "coverletter",
   description: "Generate a random cover letter response",
@@ -12,32 +11,33 @@ module.exports = {
   category: "Cover Letter",
   handler: async (ctx) => {
     const { message } = ctx;
-  
+
     try {
       // Define the user's request (you can customize this prompt)
       const userRequest = "I am applying for the position of software developer.";
-      console.info("HERE is my API Key"+openaiApiKey);
-      // Create a new instance of the OpenAI API
-      const openai = new OpenAIApi({
-        key: openaiApiKey,
+
+      // Create an instance of the OpenAI API
+      const openai = new OpenAIApi({ key: openaiApiKey });
+
+      // Define the parameters for generating a completion
+      const params = {
         engine: "davinci",
+        prompt: `Write a cover letter: ${userRequest}`,
         temperature: 0.7, // Adjust for randomness
         max_tokens: 150, // Adjust for length
-      });
-  
+      };
+
       // Generate a cover letter using OpenAI's GPT-3
-      const response = await openai.createCompletion({
-        prompt: `Write a cover letter: ${userRequest}`,
-      });
-  
+      const response = await openai.createCompletion(params);
+
       // Get the generated cover letter from the API response
       const coverLetter = response.choices[0].text;
-  
+
       // Reply with the generated cover letter
       await ctx.reply(coverLetter);
     } catch (error) {
-      console.error(error); // Log the error
-      await ctx.reply("Sorry, there was an error generating the cover letter." + error.stack);
+      console.error(error.stack); // Log the error, including the stack trace
+      await ctx.reply("Sorry, there was an error generating the cover letter: " + error.message);
     }
   },
 };
