cdf7e3e40c9e5367caa0e6457cdf64ed69557a1b
Vlad <vladsamonin@gmail.com>
2023-10-17 01:51:39 -0500

    Update coverletter.js

--- commands/coverletter.js
+++ commands/coverletter.js
@@ -31,7 +31,7 @@ module.exports = {
       const jobPostingDescription = userResponse.message.text;
 
       // Define the parameters for generating a completion
-      const response = await openai.chat.create({
+      const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages: [
           { role: 'system', content: 'You are a helpful assistant that writes cover letters.' },
