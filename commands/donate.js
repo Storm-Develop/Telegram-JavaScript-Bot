d08a428b5cbdbc54f12ea62fc219ecda7ece35cc
Vlad <vladsamonin@gmail.com>
2023-10-27 00:40:43 -0500

    Update donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -4,7 +4,7 @@ module.exports = {
   description: "Please support the chatbot to help manage server expenses & help with further development.",
   usage: "/donate",
   example: "/donate",
-  category: "Donation",
+  category: "Support",
   handler: async (ctx) => {           
     await ctx.reply(
       "*Thank you for using our service\\!* To support our server costs and further development, please consider donating\\. Click on the link: [Donate via PayPal](https://www.paypal.com/donate/?hosted_button_id=PV7HABMM9S54S)\\.",
